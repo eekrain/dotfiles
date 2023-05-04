8fbd05edc32b8b386402d7fa507037adbb0d1bef
Ardian Eka Candra <ardianoption@gmail.com>
2023-05-05 03:07:40 +0700

    use latest kernel

--- nixos/hardware/vivobook-15.nix
+++ nixos/hardware/vivobook-15.nix
@@ -1,10 +1,13 @@
-{ config, inputs, ... }:
+{ config, inputs, pkgs, ... }:
 {
   imports = [
     ./bluetooth
     inputs.hardware.nixosModules.common-cpu-amd
     inputs.hardware.nixosModules.common-pc-laptop-ssd
   ];
+
+  # use latest kernel
+  boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
 
   # IF for some reason your system can't boot up cause of bluetooth issue, add this line to add all linux firmware
   hardware.enableAllFirmware = true;
