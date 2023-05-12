c52b5dd8a4f8fab4c530507c7cb120be61f5a7e9
Ardian Eka Candra <ardianoption@gmail.com>
2023-05-12 18:48:24 +0700

    add adb

--- nixos/hardware/vivobook-15/default.nix
+++ nixos/hardware/vivobook-15/default.nix
@@ -18,5 +18,6 @@
 
   # IF for some reason your system can't boot up cause of bluetooth issue, add this line to add all linux firmware
   hardware.enableAllFirmware = true;
-
+  # for android development
+  programs.adb.enable = true;
 }
