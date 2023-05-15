6db96580f5d87b3967ee1ebc12697004a0b5e549
Ardian Eka Candra <ardianoption@gmail.com>
2023-05-15 14:52:20 +0700

    add anime4k

--- home-manager/hyprland/programs/mpv/default.nix
+++ home-manager/hyprland/programs/mpv/default.nix
@@ -5,6 +5,9 @@
       enable = true;
     };
   };
+  xdg.configFile."mpv/input.conf".source = ./input.conf;
   xdg.configFile."mpv/mpv.conf".source = ./mpv.conf;
   xdg.configFile."mpv/scripts/file-browser.lua".source = ./scripts/file-browser.lua;
+  xdg.configFile."mpv/shaders".source = ./shaders;
+  xdg.configFile."mpv/shaders".recursive = true;
 }
