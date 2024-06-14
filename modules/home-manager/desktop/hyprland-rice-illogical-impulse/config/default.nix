91c63cdd4ef07221291fb1ebdff1921cd70b3a4d
Ardian Eka Candra <ardianoption@gmail.com>
2024-06-14 17:05:39 +0700

    init wallpaper with custom scripts because .gif is broken

--- modules/home-manager/desktop/hyprland-rice-illogical-impulse/config/default.nix
+++ modules/home-manager/desktop/hyprland-rice-illogical-impulse/config/default.nix
@@ -1,10 +1,25 @@
-{ inputs, config, lib, pkgs, ... }:
-with lib;
-let
-  cfg = config.myHmModules.desktop.hyprland;
-in
 {
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.myHmModules.desktop.hyprland;
+  # I dont know why gif wallpaper when it's cached it's just stuck from booting
+  # So i will do it manually
+  initMyWallpaper = pkgs.writeShellScriptBin "initMyWallpaper" ''
+    swww clear
+    sleep 1
+    swww img ~/Pictures/wallpapers/1.jpg --transition-type grow --transition-pos "$(hyprctl cursorpos)" --transition-duration 3
+    sleep 5
+    # then sets my favorite .gif wallpaper
+    swww img ~/Pictures/wallpapers/misono-mika-angel-blue-archive-moewalls.gif --transition-type grow --transition-pos "$(hyprctl cursorpos)" --transition-duration 3
+  '';
+in {
   config = mkIf (cfg.riceSetup == "hyprland-rice-illogical-impulse") {
+    home.packages = [initMyWallpaper]; #install custom script
     # Install the default .config for configuration reference
     home.file.".config.example" = {
       source = "${pkgs.my-illogical-impulse-dots}/.config";
@@ -18,11 +33,13 @@ in
       ];
 
       # FIXME please copy all files inside dotfiles/modules/home-manager/desktop/hyprland-rice-illogical-impulse/config
-      # except this default.nix into your ~/.config 
+      # except this default.nix into your ~/.config
       # because there is a config in which it cant be managed by home-manager
       extraConfig = ''
         # This config sources other files in `hyprland` and `custom` folders
         # You wanna add your stuff in file in `custom`
+        exec-once = swww-daemon
+        exec-once = initMyWallpaper
 
         # Defaults
         source=~/.config/hypr/hyprland/env.conf
@@ -37,7 +54,7 @@ in
     services.cliphist = {
       enable = true;
       allowImages = true;
-      extraOptions = [ "-max-dedupe-search" "10" "-max-items" "500" ];
+      extraOptions = ["-max-dedupe-search" "10" "-max-items" "500"];
     };
   };
 }
