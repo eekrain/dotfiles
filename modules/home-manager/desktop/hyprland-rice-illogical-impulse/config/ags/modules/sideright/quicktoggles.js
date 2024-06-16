a2b16912448917f80ab8e821405e0b14fdee5e89
Ardian Eka Candra <ardianoption@gmail.com>
2024-06-16 22:37:24 +0700

    add hardcoded latitude & longitude to gammastep, cus geoclue seems not working for me

--- modules/home-manager/desktop/hyprland-rice-illogical-impulse/config/ags/modules/sideright/quicktoggles.js
+++ modules/home-manager/desktop/hyprland-rice-illogical-impulse/config/ags/modules/sideright/quicktoggles.js
@@ -89,7 +89,7 @@ export const ModuleNightLight = async (props = {}) => {
         onClicked: (self) => {
             self.attribute.enabled = !self.attribute.enabled;
             self.toggleClassName('sidebar-button-active', self.attribute.enabled);
-            if (self.attribute.enabled) Utils.execAsync('gammastep').catch(print)
+            if (self.attribute.enabled) Utils.execAsync('gammastep -l -7.87538:110.42621').catch(print)
             else Utils.execAsync('pkill gammastep')
                 .then(() => {
                     // disable the button until fully terminated to avoid race
