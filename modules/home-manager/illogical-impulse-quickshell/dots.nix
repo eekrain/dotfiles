14bbfbe1505c7ad9001c91cf1c17a009cfcb0421
Ardian Eka Candra <ardianoption@gmail.com>
2025-09-28 00:23:30 +0700

    test

--- modules/home-manager/illogical-impulse-quickshell/dots.nix
+++ modules/home-manager/illogical-impulse-quickshell/dots.nix
@@ -5,9 +5,9 @@
   pkgs,
   ...
 }: {
-  # Copy all the files from dots folder
+  # Copy all the files from this dots folder
   # For applying the config we still need to copy over manually not nix managed
-  # because illogical impulse's quickshell needs some write access
+  # because quickshell needs some write access
   home.file.".config.example-quickshell" = {
     source = ./dots;
     executable = true; # make all files executable
@@ -18,11 +18,11 @@
     # It copies from the reference dot files located ~/.config.example-quickshell as we already set above
     # Run it from cli "init-illogical-impulse-quickshell"
     (pkgs.writeShellScriptBin "init-illogical-impulse-quickshell" ''
-      mkdir -p ~/.config/quickshell && rsync -avz ~/.config.example-quickshell/quickshell ~/.config --chmod=Du=rwx,Dg=rx,D=rx,Fu=rwx,Fg=rx,Fo=rx
-      mkdir -p ~/.config/hypr && rsync -avz ~/.config.example-quickshell/hyprland ~/.config/hypr --chmod=Du=rwx,Dg=rx,D=rx,Fu=rwx,Fg=rx,Fo=rx
-      mkdir -p ~/.config/foot && rsync -avz ~/.config.example-quickshell/applications/foot.ini ~/.config/foot/foot.ini --chmod=Du=rwx,Dg=rx,D=rx,Fu=rwx,Fg=rx,Fo=rx
-      # Starting quickshell
-      pidof qs || qs &
+      mkdir -p ~/.config/hypr/hyprland && rsync -avz ~/.config.example-quickshell/hyprland ~/.config/hypr --chmod=Du=rwx,Dg=rx,D=rx,Fu=rwx,Fg=rx,Fo=rx
+      rsync -avz ~/.config.example-quickshell/quickshell ~/.config --chmod=Du=rwx,Dg=rx,D=rx,Fu=rwx,Fg=rx,Fo=rx
+      rsync -avz ~/.config.example-quickshell/applications ~/.config --chmod=Du=rwx,Dg=rx,D=rx,Fu=rwx,Fg=rx,Fo=rx
+      # Starting hyprland
+      pidof Hyprland || Hyprland
     '')
   ];
 }
