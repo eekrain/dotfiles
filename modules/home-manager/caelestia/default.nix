1bd5a3fee10ef5e4f9ab6049e6e1f963de032d09
Ardian Eka Candra <ardianoption@gmail.com>
2025-09-29 16:05:37 +0000

    add hypr dots

--- modules/home-manager/caelestia/default.nix
+++ modules/home-manager/caelestia/default.nix
@@ -21,55 +21,61 @@
     };
   };
 
-  # Integration with UWSM
-  xdg.configFile."uwsm/env" = {
-    executable = true;
-    text = ''
-      # Caelestia specific environment variables
-      export XDG_CURRENT_DESKTOP="Caelestia"
-      export XDG_SESSION_DESKTOP="Caelestia"
 
-      # Toolkit backend variables
-      export GDK_BACKEND="wayland,x11,*"
-      export QT_QPA_PLATFORM="wayland;xcb"
-      export SDL_VIDEODRIVER="wayland"
-      export CLUTTER_BACKEND="wayland"
-
-      # Theming variables
-      export GTK_THEME="Nordic"
-      export XCURSOR_THEME="BreezeX-RoséPine"
-      export XCURSOR_SIZE="24"
-
-      # Qt platform theme
-      export QT_QPA_PLATFORMTHEME="qt5ct"
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-
-      # NixOS specific
-      export NIXOS_OZONE_WL="1"
-      export ELECTRON_OZONE_PLATFORM_HINT="wayland"
-
-      # XDG specifications
-      export XDG_SESSION_TYPE="wayland"
-      export XDG_CURRENT_DESKTOP="Hyprland"
-      export XDG_SESSION_DESKTOP="Hyprland"
-
-      # Others
-      export EDITOR="code"
-      export BROWSER="zen"
-      export TERMINAL="kitty"
-      export LIBSEAT_BACKEND="logind"
-      export GDK_SCALE="1"
-      export MOZ_ENABLE_WAYLAND="1"
-      export _JAVA_AWT_WM_NONREPARENTING="1"
-
-      # Additional Qt settings
-      export QT_AUTO_SCREEN_SCALE_FACTOR="1"
-      export QT_SCALE_FACTOR="1"
-      export HYPRCURSOR_THEME="rose-pine-hyprcursor"
-      export HYPRCURSOR_SIZE="24"
-
-      export CAELESTIA_THEME="nordic";
-      export CAELESTIA_ACCENT="blue";
-    '';
+  xdg.configFile."hypr" = {
+    source = ./hypr; # Assuming you have a directory named nvim in the same directory
+    recursive = true; # This is important for directories
   };
+
+  # Integration with UWSM
+  # xdg.configFile."uwsm/env" = {
+  #   executable = true;
+  #   text = ''
+  #     # Caelestia specific environment variables
+  #     export XDG_CURRENT_DESKTOP="Caelestia"
+  #     export XDG_SESSION_DESKTOP="Caelestia"
+
+  #     # Toolkit backend variables
+  #     export GDK_BACKEND="wayland,x11,*"
+  #     export QT_QPA_PLATFORM="wayland;xcb"
+  #     export SDL_VIDEODRIVER="wayland"
+  #     export CLUTTER_BACKEND="wayland"
+
+  #     # Theming variables
+  #     export GTK_THEME="Nordic"
+  #     export XCURSOR_THEME="BreezeX-RoséPine"
+  #     export XCURSOR_SIZE="24"
+
+  #     # Qt platform theme
+  #     export QT_QPA_PLATFORMTHEME="qt5ct"
+  #     export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+
+  #     # NixOS specific
+  #     export NIXOS_OZONE_WL="1"
+  #     export ELECTRON_OZONE_PLATFORM_HINT="wayland"
+
+  #     # XDG specifications
+  #     export XDG_SESSION_TYPE="wayland"
+  #     export XDG_CURRENT_DESKTOP="Hyprland"
+  #     export XDG_SESSION_DESKTOP="Hyprland"
+
+  #     # Others
+  #     export EDITOR="code"
+  #     export BROWSER="zen"
+  #     export TERMINAL="kitty"
+  #     export LIBSEAT_BACKEND="logind"
+  #     export GDK_SCALE="1"
+  #     export MOZ_ENABLE_WAYLAND="1"
+  #     export _JAVA_AWT_WM_NONREPARENTING="1"
+
+  #     # Additional Qt settings
+  #     export QT_AUTO_SCREEN_SCALE_FACTOR="1"
+  #     export QT_SCALE_FACTOR="1"
+  #     export HYPRCURSOR_THEME="rose-pine-hyprcursor"
+  #     export HYPRCURSOR_SIZE="24"
+
+  #     export CAELESTIA_THEME="nordic";
+  #     export CAELESTIA_ACCENT="blue";
+  #   '';
+  # };
 }
